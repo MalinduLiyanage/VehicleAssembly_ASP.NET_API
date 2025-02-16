4e2df4e5bc0f420eb6763d6bf05d8b64125a57e2
MalinduLiyanage <malindumadhubashana@gmail.com>
2025-02-16 16:10:09 +0530

    Add project files.

---
+++ Vehicle_Assembly/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    // Define your DbSet properties for tables
+    public DbSet<MyModel> MyModels { get; set; }
+}
+
+// Define the model class
+public class MyModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ Vehicle_Assembly/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vehicle_Assembly.Controllers;
+[Route("api/DB[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public TestController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult TestDatabase()
+    {
+        return Ok(_context.Database.CanConnect());
+    }
+}
+
